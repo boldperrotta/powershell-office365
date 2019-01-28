5e9de92b0a11b920f60ff2f12a2d3abef488b90c
boldperrotta <39933592+boldperrotta@users.noreply.github.com>
2019-01-28 08:36:43 -0500

    Update Baseline
    Added blocking uncommon attachmnets

--- baseline/Office365Baseline.ps1
+++ baseline/Office365Baseline.ps1
@@ -79,6 +79,18 @@ do
                     Set-TransportRule -Identity $ruleName -Priority 0 -FromScope "NotInOrganization" -ApplyHtmlDisclaimerLocation "Prepend" `
                          -HeaderMatchesMessageHeader From -HeaderMatchesPatterns $displayNames -ApplyHtmlDisclaimerText $ruleHtml
                }
+           } '8' {
+               $ruleName = "Block Uncommon Attachment Extensions"
+               $rule = Get-TransportRule | Where-Object {$_.Identity -contains $ruleName}
+ 
+               if (!$rule) {
+                   Write-Host "Rule not found, creating rule" -ForegroundColor Green
+                   New-TransportRule -Name $ruleName -Priority 0 -FromScope "NotInOrganization" -SentTo "Chris Toppin" -AttachmentExtensionMatchesWords .adp,.app,.asp,.bas,.bat,.cer,.chm,.cmd,.cnt,.com,.cpl,.crt,.csh,.der,.exe,.fxp,.gadget,.hlp,.hpj,.hta,.inf,.ins,.isp,.its,.js,.jse,.ksh,.lnk,.mad,.maf,.mag,.mam,.maq,.mar,.mas,.mat,.mau,.mav,.maw,.mda,.mdb,.mde,.mdt,.mdw,.mdz,.msc,.msh,.msh1,.msh2,.mshxml,.msh1xml,.msh2xml,.msi,.msp,.mst,.ops,.osd,.pcd,.pif,.plg,.prf,.prg,.pst,.reg,.scf,.scr,.sct,.shb,.shs,.ps1,.ps1xml,.ps2,.ps2xml,.psc1,.psc2,.tmp,.url,.vb,.vbe,.vbp,.vbs,.vsmacros,.vsw,.ws,.wsc,.wsf,.wsh,.xnk,.ade,.cla,.class,.grp,.jar,.mcf,.ocx,.pl,.xbap -DeleteMessage $true
+                           }
+               else {
+                   Write-Host "Rule found, updating rule" -ForegroundColor Green
+                   Set-TransportRule -Identity $ruleName -Priority 0 -FromScope "NotInOrganization" -AttachmentExtensionMatchesWords .adp,.app,.asp,.bas,.bat,.cer,.chm,.cmd,.cnt,.com,.cpl,.crt,.csh,.der,.exe,.fxp,.gadget,.hlp,.hpj,.hta,.inf,.ins,.isp,.its,.js,.jse,.ksh,.lnk,.mad,.maf,.mag,.mam,.maq,.mar,.mas,.mat,.mau,.mav,.maw,.mda,.mdb,.mde,.mdt,.mdw,.mdz,.msc,.msh,.msh1,.msh2,.mshxml,.msh1xml,.msh2xml,.msi,.msp,.mst,.ops,.osd,.pcd,.pif,.plg,.prf,.prg,.pst,.reg,.scf,.scr,.sct,.shb,.shs,.ps1,.ps1xml,.ps2,.ps2xml,.psc1,.psc2,.tmp,.url,.vb,.vbe,.vbp,.vbs,.vsmacros,.vsw,.ws,.wsc,.wsf,.wsh,.xnk,.ade,.cla,.class,.grp,.jar,.mcf,.ocx,.pl,.xbap -DeleteMessage $true
+                    }
            } 'a' {
                 cls
                 'You selected option A, please follow and agree to all prompts  to install the AzureAD Module'
